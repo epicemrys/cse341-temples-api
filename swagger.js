f582cb2830637356f0337473dc0f13b1ac8b59e7
epicemrys <emrysweb@outlook.comm>
2025-03-15 06:18:49 +0100

    Fixing error 1.1

--- swagger.js
+++ swagger.js
@@ -15,7 +15,7 @@ const endpointsFiles = ['./routes/index.js'];
 // generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
 
-// Run server after it gets generated
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+  // Run server after it gets generated
+//   swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
 //   await import('./index.js');
 // });
